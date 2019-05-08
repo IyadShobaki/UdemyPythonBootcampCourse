2a7f7962a6508fbe1d330655c8dbd363103f78ec
Iyad Shobaki <iyadshobaki82@gmail.com>
2019-05-08 19:42:16 -0400

    Third commit

---
+++ TextEditor/NameAndMain/one.py
@@ -0,0 +1,17 @@
+#one.py
+
+def func():
+	print("Func() in one.py")
+
+#print("Top level in one.py")
+def function():
+	pass
+def function2():
+	pass
+if __name__ == '__main__':
+# 	print('one.py is being run directly!')
+# else:
+# 	print('one.py has been imported')
+		# Run the script!
+		function()
+		function2()
